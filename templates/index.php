45a2d0c34ca66a687ad1a46992cc40da2c2e42ea
Hao Wang <fjqz50@gmail.com>
2017-04-21 16:32:58 -0400

    update the view

--- templates/index.php
+++ templates/index.php
@@ -6,167 +6,219 @@
     <title>Wiki Search</title>
     <!-- Bootstrap core CSS -->
     <!-- <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"> -->
-    <link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
-        
+
 
     <style type="text/css">
       body {
-    padding-top: 50px;
-}
-.dropdown.dropdown-lg .dropdown-menu {
-    margin-top: -1px;
-    padding: 6px 20px;
-}
-.input-group-btn .btn-group {
-    display: flex !important;
-}
-.btn-group .btn {
-    border-radius: 0;
-    margin-left: -1px;
-}
-.btn-group .btn:last-child {
-    border-top-right-radius: 4px;
-    border-bottom-right-radius: 4px;
-}
-.btn-group .form-horizontal .btn[type="submit"] {
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
-}
-.form-horizontal .form-group {
-    margin-left: 0;
-    margin-right: 0;
-}
-.form-group .form-control:last-child {
-    border-top-left-radius: 4px;
-    border-bottom-left-radius: 4px;
-}
+                padding-top: 50px;
+            }
+            .dropdown.dropdown-lg .dropdown-menu {
+                margin-top: -1px;
+                padding: 6px 20px;
+            }
+            .input-group-btn .btn-group {
+                display: flex !important;
+            }
+            .btn-group .btn {
+                border-radius: 0;
+                margin-left: -1px;
+            }
+            .btn-group .btn:last-child {
+                border-top-right-radius: 4px;
+                border-bottom-right-radius: 4px;
+            }
+            .btn-group .form-horizontal .btn[type="submit"] {
+              border-top-left-radius: 4px;
+              border-bottom-left-radius: 4px;
+            }
+            .form-horizontal .form-group {
+                margin-left: 0;
+                margin-right: 0;
+            }
+            .form-group .form-control:last-child {
+                border-top-left-radius: 4px;
+                border-bottom-left-radius: 4px;
+            }
 
-@media screen and (min-width: 768px) {
-    #adv-search {
-        width: 500px;
-        margin: 0 auto;
-    }
-    .dropdown.dropdown-lg {
-        position: static !important;
-    }
-    .dropdown.dropdown-lg .dropdown-menu {
-        min-width: 500px;
-    }
-}
+            @media screen and (min-width: 768px) {
+                #adv-search {
+                    width: 500px;
+                    margin: 0 auto;
+                }
+                .dropdown.dropdown-lg {
+                    position: static !important;
+                }
+                .dropdown.dropdown-lg .dropdown-menu {
+                    min-width: 500px;
+                }
+            }
+            /* Style the tab */
+            div.tab {
+                overflow: hidden;
+                border: 1px solid #ccc;
+                background-color: #f1f1f1;
+            }
 
+            /* Style the buttons inside the tab */
+            div.tab button {
+                background-color: inherit;
+                float: left;
+                border: none;
+                outline: none;
+                cursor: pointer;
+                padding: 14px 16px;
+                transition: 0.3s;
+            }
 
+            /* Change background color of buttons on hover */
+            div.tab button:hover {
+                background-color: #ddd;
+            }
+
+            /* Create an active/current tablink class */
+            div.tab button.active {
+                background-color: #ccc;
+            }
+
+            /* Style the tab content */
+            .tabcontent {
+                display: none;
+                padding: 6px 12px;
+                border: 1px solid #ccc;
+                border-top: none;
+            }
 
     </style>
-<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script> 
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="http://cdn.bootcss.com/twitter-bootstrap/3.0.1/js/bootstrap.min.js"></script>
 
     <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type='text/javascript'></script> -->
 </head>
 
-<body role="document">
+<body role="document" style="background-image: url(http://wallpapercave.com/wp/hFSZpWV.jpg)">
+<div class="container theme-showcase" role="main">
+<!-- add tab -->
+  <div class="tab">
+  <button class="tablinks" onclick="openCity(event, 'London')">Basic</button>
+  <button class="tablinks" onclick="openCity(event, 'Paris')">Advanced</button>
+  <button class="tablinks" onclick="openCity(event, 'Tokyo')">Custom</button>
+  </div>
+
+  <div id="London" class="tabcontent" style=display:block>
+    <div class="jumbotron">
+     <h1>Wiki Search!</h1>
 
 
-
- 
-
-
-
-
-    <div class="container theme-showcase" role="main">
-     
-
-      <div class="jumbotron">
-       <h1>Wiki Search!</h1>
-       
-         
-       <div style="padding: 10px 100px 10px;">
-  <form class="bs-example bs-example-form" role="form" action="results" method="post">
-    <div class="input-group">
-      <span class="input-group-addon">query</span>
-      <input type="text" class="form-control" name="inputValue">
+     <div style="padding: 10px 100px 10px;">
+<form class="bs-example bs-example-form" role="form" action="results" method="post">
+  <div class="input-group">
+    <span class="input-group-addon">query</span>
+    <input type="text" class="form-control" name="inputValue">
+  </div>
+  <br>
+  <div class="input-group">
+    <span class="input-group-addon">starring</span>
+    <input type="text" class="form-control" name="starValue">
+  </div>
+  <br>
+  <div class="input-group">
+    <span class="input-group-addon">runtime</span>
+    <div>
+    <input type="text" class="form-control" name="runValue1">
+    <input type="text" class="form-control" name="runValue2">
     </div>
-    <br>
-    <div class="input-group">
-      <span class="input-group-addon">starring</span>
-      <input type="text" class="form-control" name="starValue">
-    </div>
-    <br>
-    <div class="input-group">
-      <span class="input-group-addon">runtime</span>
-      <div>
-      <input type="text" class="form-control" name="runValue1">
-      <input type="text" class="form-control" name="runValue2">
-      </div>
-    </div>
-    <br>
-    <div class="input-group">
-      <select class="form-control" name="genreValue">
-              <option value="default">default</option>
-              <option value="comedy">comedy</option>
-              <option value="romance">romance</option>
-              <option value="thriller">thriller</option>
-              <option value="mystery">mystery</option>
-              <option value="crime">crime</option>
-              <option value="drama">drama</option>
-              <option value="musical">musical</option>
-              <option value="documentary">documentary</option>
-              <option value="western">western</option>
-              <option value="animation">animation</option>
-            </select>
-    </div>
-    <br>
-    <button class="btn btn-primary" type="submit" >Search</button> 
-  </form>
+  </div>
+  <br>
+  <div class="input-group">
+    <select class="form-control" name="genreValue">
+            <option value="default">default</option>
+            <option value="comedy">comedy</option>
+            <option value="romance">romance</option>
+            <option value="thriller">thriller</option>
+            <option value="mystery">mystery</option>
+            <option value="crime">crime</option>
+            <option value="drama">drama</option>
+            <option value="musical">musical</option>
+            <option value="documentary">documentary</option>
+            <option value="western">western</option>
+            <option value="animation">animation</option>
+          </select>
+  </div>
+  <br>
+  <button class="btn btn-primary" type="submit" >Search</button>
+</form>
 </div>
-     
-      <div class="container">
-  <div class="row">
-    <div class="col-md-12">
-            <div class="input-group" id="adv-search">
-                <input type="text" class="form-control" placeholder="Search for snippets" />
-                <div class="input-group-btn">
-                    <div class="btn-group" role="group">
-                        <div class="dropdown dropdown-lg">
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="caret"></span></button>
-                            <div class="dropdown-menu dropdown-menu-right" role="menu">
-                                <form class="form-horizontal" role="form">
-                                  <div class="form-group">
-                                    <label for="filter">Filter by</label>
-                                    <select class="form-control">
-                                        <option value="0" selected>All Snippets</option>
-                                        <option value="1">Featured</option>
-                                        <option value="2">Most popular</option>
-                                        <option value="3">Top rated</option>
-                                        <option value="4">Most commented</option>
-                                    </select>
-                                  </div>
-                                  <div class="form-group">
-                                    <label for="contain">Author</label>
-                                    <input class="form-control" type="text" />
-                                  </div>
-                                  <div class="form-group">
-                                    <label for="contain">Contains the words</label>
-                                    <input class="form-control" type="text" />
-                                  </div>
-                                  <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-                                </form>
-                            </div>
-                        </div>
-                        <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-                    </div>
-                </div>
-            </div>
+
+    <div class="container">
+<div class="row">
+  <div class="col-md-12">
+          <div class="input-group" id="adv-search">
+              <input type="text" class="form-control" placeholder="Search for snippets" />
+              <div class="input-group-btn">
+                  <div class="btn-group" role="group">
+                      <div class="dropdown dropdown-lg">
+                          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="caret"></span></button>
+                          <div class="dropdown-menu dropdown-menu-right" role="menu">
+                              <form class="form-horizontal" role="form">
+                                <div class="form-group">
+                                  <label for="filter">Filter by</label>
+                                  <select class="form-control">
+                                      <option value="0" selected>All Snippets</option>
+                                      <option value="1">Featured</option>
+                                      <option value="2">Most popular</option>
+                                      <option value="3">Top rated</option>
+                                      <option value="4">Most commented</option>
+                                  </select>
+                                </div>
+                                <div class="form-group">
+                                  <label for="contain">Author</label>
+                                  <input class="form-control" type="text" />
+                                </div>
+                                <div class="form-group">
+                                  <label for="contain">Contains the words</label>
+                                  <input class="form-control" type="text" />
+                                </div>
+                                <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                              </form>
+                          </div>
+                      </div>
+                      <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                  </div>
+              </div>
           </div>
         </div>
-  </div>
+      </div>
 </div>
+</div>
+  </div>
+
+  <div id="Paris" class="tabcontent">
+  <h3>Paris</h3>
+  <p>Paris is the capital of France.</p>
+  </div>
+
+  <div id="Tokyo" class="tabcontent">
+  <h3>Tokyo</h3>
+  <p>Tokyo is the capital of Japan.</p>
+  </div>
+
+
+<!-- end of tab -->
+
+
+
+
+
+
+
      <div class="jumbotron result_num stop_word stop_flag unknown unknown_flag">
 
       <p><small>About {{result_num}} results</small>
-      
+
       {% if stop_flag %}
-      <small>Ignoring term: 
+      <small>Ignoring term:
       {% for s in stop_word %}
             {{s}}
       {% endfor %}
@@ -175,7 +227,7 @@
       {%endif%}
 
       {% if unknown_flag %}
-      <small>Unknown search term: 
+      <small>Unknown search term:
       {% for s in unknown %}
             {{s}}
       {% endfor %}
@@ -184,12 +236,17 @@
       {%endif%}
 
       </p>
-       <form action="showarticle.php" method="get">
+
+       </div>
+
+<!--  -->
+</div>
+
       <!-- <table class="table table-striped table-bordered table-hover result stop_word">
         <tbody>
         {% for res in result %}
         <tr><td>
-        
+
             <p><strong>{{res[11]}}. {{res[0]}}({{res[12]}})</strong></p>
 
             <p>{{res[1]}}
@@ -200,13 +257,13 @@
           </form>
 
           </td></tr>
-          
+
           {% endfor %}
-          
+
         </tbody>
       </table>
-       </form> 
-      
+       </form>
+
         <ul class="pager result_page">
         <li>
             <a>next</a>
@@ -225,11 +282,11 @@
           </ul>
           <form action="jump_prev" name="PPrev" id="PPrev" method="post">
               <input type="hidden", value="Prev">
-              
+
           </form>
           <form action="jump_next" name="NNext" id="NNext" method="post">
               <input type="hidden", value="Next">
-             
+
             </form>
 
           <ul class="pager">
@@ -240,7 +297,7 @@
             <a href='javascript:my_submit("NNext")'>Next</a>
             </li>
         </ul>
-      </div>        
+      </div>
     </div>   -->
 
 
@@ -248,19 +305,40 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    
+
     <script type="text/javascript">
       $(function() {
-        $('.pagination li a').click(function() { 
+        $('.pagination li a').click(function() {
             dstpage = $(this).data('page');
             if (dstpage == 0)
-                return; 
+                return;
             $('#idCurPage').val(dstpage);
             cur = $('#idCurPage').val();
             $('form').submit();
             return false;
         });
     });
+    // tab function
+    function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
     </script>
 </body>
 </html>
