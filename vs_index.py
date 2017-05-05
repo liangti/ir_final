4286b04b683bd65962c38c4adddff6e07415a374
Liang Ti <469140303@qq.com>
2017-05-04 21:18:57 -0400

    update

--- vs_index.py
+++ vs_index.py
@@ -52,9 +52,16 @@ create_body= {
                     "type":"text",
                     "analyzer":"my_synonym"
                     
-                },        
+                }
+                          ,        
                 "Ing_Name" :{
                     "type": "nested",
+                    "properties":{
+                    "Name":{"type": "text"},    
+                    "Unit":{"type": "text"},   
+                    "Quantity":{"type": "float"},
+                    "PreparationNotes":{"type": "text"}    
+                    },
                     "analyzer":"my_synonym",
                 }               
             }
@@ -67,7 +74,7 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 corpus='reciption2'
 
-# es.indices.delete(index=corpus)
+es.indices.delete(index=corpus)
 es.indices.create(index=corpus, ignore=400, body=create_body)
 
 with open("data_test.json", 'r') as file:
@@ -78,9 +85,9 @@ for item in data:
     ing_list=data[item]['Ingredients']
     ing_name=[]
     for ing in ing_list:
-        ing_name.append([ing['Name'],ing['Unit'],ing['Quantity'],ing['PreparationNotes']])
+        ing_name.append([ing['Name'],ing['Unit'],str(ing['Quantity']),ing['PreparationNotes']])
     data[item]['Ing_Name']=ing_name
-    print ing_name
+#     print ing_name
 
 action=[]
 for item in data:
@@ -89,9 +96,9 @@ for item in data:
     print data[item]['Title']
     print len(action)
     if len(action)==500:
-        helpers.bulk(es, action, index=corpus, doc_type="food_type", stats_only=True)
+        helpers.bulk(es, action, index=corpus, doc_type="food_type")
         action=[]
         
 if len(action)!=0:
-    helpers.bulk(es, action, index=corpus, doc_type="food_type", stats_only=True)
+    helpers.bulk(es, action, index=corpus, doc_type="food_type")
 action=[]
